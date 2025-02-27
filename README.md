6c0ba985feaabf55429ba3e82ad476cce07285d0
yehengyu <yehengyu@bytedance.com>
2025-02-27 23:11:38 +0800

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+# Fremer
+
+This is the official code repository of the paper: Fremer: Lightweight and Effective Frequency Transformer for Workload Forecasting in Cloud Services.
+
+# Model Architecture
+![Fremer](figs/Fremer_model.jpg)
+
+### The Frequency Resolution Mis-alignment Problem:
+![Fremer](figs/FM.jpg)
+
+# Usage
+1) Prepare the Data
+
+https://huggingface.co/datasets/ByteDance/CloudTimeSeriesData
+
+put the data under `./dataset/forecasting`
+
+2) Run the Scripts under `./scripts/multivariate_forecast`
+
+Coming soon ......
+
+# Acknowledgement
+
+Special thanks to TFB (https://github.com/decisionintelligence/TFB), which supports the evaluation part of this study.
---
+++ ts_benchmark/baselines/time_series_library/__init__.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+__all__ = [
+    "Autoformer",
+    "Crossformer",
+    "DLinear",
+    "ETSformer",
+    "FEDformer",
+    "FiLM",
+    "Informer",
+    "iTransformer",
+    "Koopa",
+    "LightTS",
+    "Linear",
+    "MICN",
+    "NLinear",
+    "Nonstationary_Transformer",
+    "PatchTST",
+    "Pyraformer",
+    "Reformer",
+    "TimesNet",
+    "Transformer",
+    "Triformer",
+    "TimeMixer",
+    "Fremer1",
+    "Fremer2",
+    "Fremer3",
+    "Fremer4",
+    "Fremer5",
+    "Fremer6"
+]
+
+
+from ts_benchmark.baselines.time_series_library.models.Autoformer import Autoformer
+from ts_benchmark.baselines.time_series_library.models.Crossformer import Crossformer
+from ts_benchmark.baselines.time_series_library.models.DLinear import DLinear
+from ts_benchmark.baselines.time_series_library.models.ETSformer import ETSformer
+from ts_benchmark.baselines.time_series_library.models.FEDformer import FEDformer
+from ts_benchmark.baselines.time_series_library.models.FiLM import FiLM
+from ts_benchmark.baselines.time_series_library.models.Informer import Informer
+from ts_benchmark.baselines.time_series_library.models.iTransformer import iTransformer
+from ts_benchmark.baselines.time_series_library.models.Koopa import Koopa
+from ts_benchmark.baselines.time_series_library.models.LightTS import LightTS
+from ts_benchmark.baselines.time_series_library.patchs.Linear import Linear
+from ts_benchmark.baselines.time_series_library.models.MICN import MICN
+from ts_benchmark.baselines.time_series_library.patchs.NLinear import NLinear
+from ts_benchmark.baselines.time_series_library.models.Nonstationary_Transformer import Nonstationary_Transformer
+from ts_benchmark.baselines.time_series_library.models.PatchTST import PatchTST
+from ts_benchmark.baselines.time_series_library.models.Pyraformer import Pyraformer
+from ts_benchmark.baselines.time_series_library.models.Reformer import Reformer
+from ts_benchmark.baselines.time_series_library.models.TimesNet import TimesNet
+from ts_benchmark.baselines.time_series_library.models.Transformer import Transformer
+from ts_benchmark.baselines.time_series_library.patchs.Triformer import Triformer
+from ts_benchmark.baselines.time_series_library.models.TimeMixer import TimeMixer
+from ts_benchmark.baselines.time_series_library.models.Fremer1 import Fremer1
+from ts_benchmark.baselines.time_series_library.models.Fremer2 import Fremer2
+from ts_benchmark.baselines.time_series_library.models.Fremer3 import Fremer3
+from ts_benchmark.baselines.time_series_library.models.Fremer4 import Fremer4
+from ts_benchmark.baselines.time_series_library.models.Fremer5 import Fremer5
+from ts_benchmark.baselines.time_series_library.models.Fremer6 import Fremer6
